eaebfc6fa5c7bbb1c89f6d94e2e1b27d9f48443a
Pouyan Shirzadian <pshirzadian26@gmail.com>
2024-08-21 11:30:19 -0400

    OT SGD results added

--- rpw-sgd.py
+++ rpw-sgd.py
@@ -2,30 +2,50 @@ import math
 import torch
 import numpy as np
 import random
-
+import os
 import matplotlib.pyplot as plt
 
 dim = 2
-rows_num = 16  # the code will generate a square of rows_num x rows_num and then tries to adjust their coordinates
+rows_num = 20  # the code will generate a square of rows_num x rows_num and then tries to adjust their coordinates
 output_size = int(math.pow(rows_num, dim))
+sample_size = 500
 radius = 0.01  # radius of the disks drawn for each center
-max_iters = 400  # maximum number of sinkhorn iterations
-reg = 0.1  # regularization parameter in sinkhorn algorithm
-max_alpha = 0.3  # maximum amount of noise in each sample
+max_iters = 500  # maximum number of sinkhorn iterations
+reg = 0.05  # regularization parameter in sinkhorn algorithm
+max_alpha = 0.4  # maximum amount of noise in each sample
 epoch_num = 200
+lr = 0.1  # learning rate
+
+path = "plots/run_"
+index = 0
+with open("plots/index.txt", 'r') as f:
+    index = int(f.read())
+with open("plots/index.txt", 'w') as f:
+    f.write(str(index + 1))
+path += str(index) + "/"
+if not os.path.exists(path):
+    os.makedirs(path, exist_ok=True)
 
 
 # Drawing the maintained output distribution
-def draw(centers):
+def draw(centers, prev, samples, epoch):
     _, ax = plt.subplots()
     for center in centers:
         circle = plt.Circle((center[0], center[1]), radius, color='k')
         ax.add_patch(circle)
-    plt.show()
+    for center in prev:
+        circle = plt.Circle((center[0], center[1]), radius, color='b', alpha=0.2)
+        ax.add_patch(circle) 
+    for center in samples:
+        circle = plt.Circle((center[0], center[1]), radius, color='r', alpha=0.2)
+        ax.add_patch(circle)
+    plt.savefig(path + "fig" + str(i) + ".png")
     plt.close()
 
 
-def sinkhorn(a, b, C):
+def sinkhorn(C):
+    a = torch.ones(C.shape[0]) / C.shape[0]
+    b = torch.ones(C.shape[1]) / C.shape[1]
     K = torch.exp(-C/reg)
     u = torch.ones_like(a)
     v = torch.ones_like(b)
@@ -35,20 +55,15 @@ def sinkhorn(a, b, C):
     return torch.matmul(torch.diag_embed(u), torch.matmul(K, torch.diag_embed(v)))
 
 
-def compute_euclidean_cost_matrix(A, B):
-    matrix = [[0 for _ in range(A.shape[0])] for _ in range(B.shape[0])]
-    for i in range(A.shape[0]):
-        for j in range(B.shape[0]):
-            matrix[i][j] = math.sqrt((A[i][0] - B[j][0]) ** 2 + (A[i][1] - B[j][1]) ** 2)
-    return torch.tensor(matrix)
-
-
+# This method draws n samples from the real distribution contaminated with alpha fraction of noise
 def sample(n):
     samples = []
     cov = [[0.01, 0], [0, 0.01]]
-    alpha = random.random() * max_alpha
+
+    alpha = random.random() * max_alpha  # amount of noise in the samples
     noise_mean_x = random.random()
     noise_mean_y = random.random()
+
     for _ in range(n):
         p = random.random()
         point = [-1, -1]
@@ -59,6 +74,7 @@ def sample(n):
             while point[0] < 0 or point[0] > 1 or point[1] < 0 or point[1] > 1:
                 point = np.random.multivariate_normal([noise_mean_x, noise_mean_y], cov, 1)[0]
         samples.append(point)
+    samples = np.array(samples)
     return torch.FloatTensor(samples)
 
 
@@ -72,10 +88,17 @@ mean_y = random.random()
 print(mean_x, mean_y)
 
 for i in range(epoch_num):
-    samples = sample(output_size)
-    draw(samples)
+    samples = sample(sample_size)
 
-    cost_matrix = compute_euclidean_cost_matrix(out_centers, samples)
+    cost_matrix = torch.cdist(out_centers, samples, p=2)
 
+    plan = sinkhorn(cost_matrix)
+    plan = plan.shape[0] * plan
 
-    draw(out_centers)
+    arrows = torch.matmul(plan, samples) - out_centers
+
+    prev_out_centers = torch.clone(out_centers)
+
+    out_centers = out_centers + lr * arrows
+
+    draw(out_centers, prev_out_centers, samples, i)
